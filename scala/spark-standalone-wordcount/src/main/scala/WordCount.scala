10592fc16a153deaf23e979e225e8f7d1f510dee
Tara Gu <tarag@google.com>
2015-07-10 12:35:28 -0400

    updated spark standalone example

--- scala/spark-standalone-wordcount/src/main/scala/WordCount.scala
+++ scala/spark-standalone-wordcount/src/main/scala/WordCount.scala
@@ -38,142 +38,94 @@ object SparkExample extends Logging {
   val COLUMN_NAME_BYTES = Bytes.toBytes("Count")
        
   def main(args: Array[String]) {
-        if (args.length < 3) {
-	   throw new Exception("Please enter input file path, output table name, and expected count as arguments")
-	}
-	val file = args(0) //file path
-	val name = args(1) //output table name
-	val expectedCount = args(2).toInt
-	val prefixName = sys.env("PREFIX")
+    if (args.length < 3) {
+      throw new Exception("Please enter input file path, output table name, and expected count as arguments")
+    }
+    val file = args(0) //file path
+    val name = args(1) //output table name
+    val expectedCount = args(2).toInt
+    val prefixName = sys.env("PREFIX")
 
-	val masterInfo = "spark://" + prefixName + "-m:7077"
-	val sc= new SparkContext(masterInfo, "WordCount") 
+    val masterInfo = "spark://" + prefixName + "-m:7077"
+    val sc= new SparkContext(masterInfo, "WordCount")
 
-	// create new table if it's not already existed
-        val tableName = TableName.valueOf(name)
-        var hbaseConfig = HBaseConfiguration.create()
-        hbaseConfig.set(TableInputFormat.INPUT_TABLE, name)
-        // broadcast a serialized config object allows us to use the same conf object among the driver and executors
+    var hbaseConfig = HBaseConfiguration.create()
+    hbaseConfig.set(TableInputFormat.INPUT_TABLE, name)
+    // broadcast a serialized config object allows us to use the same conf object among the driver and executors
     val confBroadcast = sc.broadcast(new SerializableWritable(hbaseConfig))
+    // set config object to null to prevent it to be serialized when using spark-shell
     hbaseConfig = null
-/**
-    val baos = new ByteArrayOutputStream();
-    var dos = new DataOutputStream(baos);
-    hbaseConfig.write(dos);
-    val confBytes = baos.toByteArray;
-    dos = null
-  */
-        val conn = ConnectionFactory.createConnection(confBroadcast.value.value); 
-        try {
-          val admin = conn.getAdmin()
-	   if (!admin.tableExists(tableName)) {
-	       val tableDescriptor = new HTableDescriptor(tableName)
-	       tableDescriptor.addFamily(new HColumnDescriptor(COLUMN_FAMILY))
-	       admin.createTable(tableDescriptor) 
-	   }
-	   admin.close()
-        } catch {
-          case e: Exception => e.printStackTrace;  throw e
-        } finally {
-          conn.close()
-	}
+    val conn = ConnectionFactory.createConnection(confBroadcast.value.value);
 
-	val wordCounts = sc.textFile(file).flatMap(_.split(" ")).filter(_!="").map((_,1)).reduceByKey((a,b) => a+b)
+    // create new table if it's not already existed
+    val tableName = TableName.valueOf(name)
+    try {
+      val admin = conn.getAdmin()
+      if (!admin.tableExists(tableName)) {
+	val tableDescriptor = new HTableDescriptor(tableName)
+	tableDescriptor.addFamily(new HColumnDescriptor(COLUMN_FAMILY))
+	admin.createTable(tableDescriptor)
+      }
+      admin.close()
+    } catch {
+      case e: Exception => e.printStackTrace;  throw e
+    } finally {
+      conn.close()
+    }
 
-
-        wordCounts.foreachPartition { 
- 	  partition => {
-            val config = confBroadcast.value.value
-            println(config)
-            config.foreach(println)
-	    val conn1 = ConnectionFactory.createConnection(config); 
-            val tableName1 = TableName.valueOf(name)
-            val mutator = conn1.getBufferedMutator(tableName1)	    
-	    try {
-	      partition.foreach{ wordCount => {
-	        val (word, count) = wordCount
-  	        try {
-	          mutator.mutate(new Put(Bytes.toBytes(word)).addColumn(COLUMN_FAMILY_BYTES, COLUMN_NAME_BYTES, Bytes.toBytes(count))) 
-	        } catch {
-	          // This is a possible exception we could get with BufferedMutator.mutate
-	          case retries_e: RetriesExhaustedWithDetailsException => { 
-		    retries_e.getCauses().foreach(_.printStackTrace)
-		    println("Retries: "+retries_e.getClass)
-		    throw retries_e.getCauses().get(0)
-		  }
-	          case e: Exception => println("General exception: "+ e.getClass); throw e
-	        }
-	      }   }
-	    } finally {
-	      mutator.close()
-	      conn1.close()
+    val wordCounts = sc.textFile(file).flatMap(_.split(" ")).filter(_!="").map((_,1)).reduceByKey((a,b) => a+b)
+    wordCounts.foreachPartition {
+      partition => {
+        val config = confBroadcast.value.value
+        println(config)
+        config.foreach(println)
+	val conn1 = ConnectionFactory.createConnection(config);
+        val tableName1 = TableName.valueOf(name)
+        val mutator = conn1.getBufferedMutator(tableName1)
+	try {
+	  partition.foreach{ wordCount => {
+	    val (word, count) = wordCount
+  	    try {
+	      mutator.mutate(new Put(Bytes.toBytes(word)).addColumn(COLUMN_FAMILY_BYTES, COLUMN_NAME_BYTES, Bytes.toBytes(count)))
+	    } catch {
+	      // This is a possible exception we could get with BufferedMutator.mutate
+	      case retries_e: RetriesExhaustedWithDetailsException => {
+		retries_e.getCauses().foreach(_.printStackTrace)
+		println("Retries: "+retries_e.getClass)
+		throw retries_e.getCauses().get(0)
+	      }
+	      case e: Exception => println("General exception: "+ e.getClass); throw e
 	    }
-	  }   
+	  }   }
+	} finally {
+	  mutator.close()
+	  conn1.close()
+	}
+      }
   
-	}
+    }
+    //validate table count
+    val hBaseRDD = sc.newAPIHadoopRDD(confBroadcast.value.value, classOf[TableInputFormat], classOf[org.apache.hadoop.hbase.io.ImmutableBytesWritable], classOf[org.apache.hadoop.hbase.client.Result])
+    val count = hBaseRDD.count.toInt
 
+    //cleanup
+    val connCleanup = ConnectionFactory.createConnection(confBroadcast.value.value);
+    try {
+      val admin = connCleanup.getAdmin()
+      admin.deleteTable(tableName)
+      admin.close()
+    } catch {
+      case e: Exception => e.printStackTrace; throw e
+    }
+    connCleanup.close()
 
-/**
-        wordCounts.foreachPartition { 
- 	  partition => {
-            val bais = new ByteArrayInputStream(confBytes);
-            val dis = new DataInputStream(bais);
-            val config = new HBaseConfiguration();
-            println("!!!!!!!!!!!!!!!! BEFORE READ FIELDS: CONFIG IS: ")
-            println(config)
-            config.foreach(println)
-            config.readFields(dis);
-            println("!!!!!!!!!!!!!!!! AFTER READ FIELDS: CONFIG IS: ")
-            println(config)
-            config.foreach(println)
-	    val conn1 = ConnectionFactory.createConnection(config); 
-            val tableName1 = TableName.valueOf(name)
-            val mutator = conn1.getBufferedMutator(tableName1)	    
-	    try {
-	      partition.foreach{ wordCount => {
-	        val (word, count) = wordCount
-  	        try {
-	          mutator.mutate(new Put(Bytes.toBytes(word)).addColumn(COLUMN_FAMILY_BYTES, COLUMN_NAME_BYTES, Bytes.toBytes(count))) 
-	        } catch {
-	          // This is a possible exception we could get with BufferedMutator.mutate
-	          case retries_e: RetriesExhaustedWithDetailsException => { 
-		    retries_e.getCauses().foreach(_.printStackTrace)
-		    println("Retries: "+retries_e.getClass)
-		    throw retries_e.getCauses().get(0)
-		  }
-	          case e: Exception => println("General exception: "+ e.getClass); throw e
-	        }
-	      }   }
-	    } finally {
-	      mutator.close()
-	      conn1.close()
-	    }
-	  }   
-  
-	}
-  */
-	//validate table count
-	val hBaseRDD = sc.newAPIHadoopRDD(confBroadcast.value.value, classOf[TableInputFormat], classOf[org.apache.hadoop.hbase.io.ImmutableBytesWritable], classOf[org.apache.hadoop.hbase.client.Result]) 	
-	val count = hBaseRDD.count.toInt
-
-	//cleanup
-        val connCleanup = ConnectionFactory.createConnection(confBroadcast.value.value); 
-        try {
-          val admin = connCleanup.getAdmin()
-	  admin.deleteTable(tableName)
-	  admin.close()
-        } catch {
-          case e: Exception => e.printStackTrace; throw e
-        }
-        connCleanup.close()
-
-	println("Word count = " + count)
-	if (expectedCount == count) {
-	  println("Word count success")
-	} else {
-	  println("Word count failed")
-	  System.exit(1)
-	}
-	System.exit(0)
+    println("Word count = " + count)
+    if (expectedCount == count) {
+      println("Word count success")
+    } else {
+      println("Word count failed")
+      System.exit(1)
+    }
+    System.exit(0)
   }
 }
